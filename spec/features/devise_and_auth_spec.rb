82ad5dbf0241958a371a8f71ad7abbe49ba85e0e
--get <m_elliott@live.com>
2016-07-07 15:51:10 +0100

    new app setup rspec working user devise uuid working

---
+++ spec/features/devise_and_auth_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+#APP =YAML.load_file('config/application.yml')
+
+describe 'user landing', :type => :feature do
+
+    context "new user" do
+          before do 
+            visit root_path
+          end
+          it 'should be on the unauth page' do
+            page.should have_content "Why MircoBooks"
+          end
+          it 'should be allowed to sign up' do
+            first("a[href='/users/sign_up']").click()
+            fill_in("user_email", with: "a@a.com")
+            fill_in("user_email", with: "a@a.com")
+            fill_in("user_password", with: "what-a-password")
+            fill_in("user_password_confirmation", with: "what-a-password")
+            click_button("Sign up")
+            page.should have_content "Welcome! You have signed up successfully"
+          end
+    end
+
+    context "existing user" do
+          before do
+            @current_user = FactoryGirl.create(:user)
+            visit root_path
+          end
+          it 'should be on the unauth page' do
+            page.should have_content "Why MircoBooks"
+          end
+          it 'should be allowed to log in' do
+            first("a[href='/users/sign_in']").click()
+            fill_in("user_email", with: "user_2@profitbees.com")
+            fill_in("user_password", with: "what-a-password")
+            click_button("Log in")
+            page.should have_content "Signed in successfully"
+          end
+    end
+
+end
