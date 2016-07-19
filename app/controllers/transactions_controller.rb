59bb556482c8aa666ffa7fbedc9b6db43a1287f5
--get <m_elliott@live.com>
2016-07-19 12:02:09 +0100

    edit and update trns started

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,19 @@
+class TransactionsController < ApplicationController
+
+
+  def edit 
+  	@transaction = Transaction.find(params[:id])
+  end
+
+  def update
+  	@transaction = Transaction.find(params[:id])
+  	@transaction.update_attributes(edit_trn_params)
+  end
+
+
+  def edit_trn_params
+  	params.require(:transaction).permit(:bank_description, :amount, :tax)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :transactions, only: [:edit, :update]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
